a73454d6588144da00ee31f3741954295d3dca3e
CathyCahy <cathycahy@gmail.com>
2024-06-16 13:31:34 +0300

    done

--- src/main/java/ru/skypro/employee_demo1/EmployeeController.java
+++ src/main/java/ru/skypro/employee_demo1/EmployeeController.java
@@ -2,12 +2,44 @@ package ru.skypro.employee_demo1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RequestMapping("/employee")
 @RestController
 
 
 public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Добро пожаловать в отдел кадров";
+    }
+
+    @GetMapping("/add")
+    public void add(@RequestParam String firstName, @RequestParam String lastName) {
+       employeeService.add(firstName, lastName);
+
+    }
+    @GetMapping("/remove")
+    public void remove(@RequestParam String firstName, @RequestParam String lastName) {
+        employeeService.remove(firstName,lastName);
+    }
+    @GetMapping("/find")
+    public Employee find(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.find(firstName,lastName);
+
+    }@GetMapping("/all")
+    public Collection<Employee> getAll() {
+        return employeeService.getAll();
+
+    }
 
 }
--- src/main/java/ru/skypro/employee_demo1/EmployeeService.java
+++ src/main/java/ru/skypro/employee_demo1/EmployeeService.java
@@ -2,60 +2,72 @@ package ru.skypro.employee_demo1;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 
 public class EmployeeService {
-    List<Employee> employeeList = List.of(
+    List<Employee> employeeList = new ArrayList<>(List.of(
             new Employee("Иван", "Иванов"),
             new Employee("Петр", "Петров"),
-            new Employee("Павел", "Дуров")
-    );
+            new Employee("Павел", "Дуров"),
+            new Employee("Валерий", "Дуров"),
+            new Employee("Сергей", "Зверев")
+//            new Employee("Василий", "Васильев"),
+//            new Employee("Иннокентий", "Старцев"),
+//            new Employee("Люси", "Маклин"),
+//            new Employee("Куп", "Говард"),
+//            new Employee("Павел", "Дуров"),
+//            new Employee("Максимус", "Рыцарь"),
+//            new Employee("Ли", "Молдэйвер"),
+//            new Employee("Барб", "Говард"),
+//            new Employee("Стеф", "Харпер"),
+//            new Employee("Бетти", "Пирсон")
+    ));
 
-    int maxEmployee = 15;
+    int maxEmployee = 14;
 
-    public void addEmployee(String firstName, String lastName) {
-
+    public void add(String firstName, String lastName) {
+        Employee emp = new Employee(firstName, lastName);
         if (employeeList.size() >= maxEmployee) {
             // System.out.println("Нельзя добавить сотрудника, штат заполнен");
             throw new EmployeeStorageIsFullException();
         }
-        Employee emp = new Employee(firstName, lastName);
-        if (findEmployee(firstName, lastName)) {
+        if (employeeList.contains(emp)) {
             throw new EmployeeAlreadyAddedException();
         }
-
-        employeeList.add(employeeList.size() + 1, new Employee(firstName, lastName));
-
+        employeeList.add(emp);
     }
 
-    public void removeEmployee(String firstName, String lastName) {
-        Employee emp = new Employee(firstName,lastName);
-        for (int i = 0; i < employeeList.size(); i++) {
-
-            if (employeeList.get(i).equals(emp)) {
-                employeeList.remove(employeeList.get(i));
-            }
-            if (!findEmployee(firstName,lastName)) {
-                throw new EmployeeNotFoundException();
-            }
-        }
-    }
-
-    public boolean findEmployee(String firstName, String lastName) {
+    public void remove(String firstName, String lastName) {
         Employee emp = new Employee(firstName, lastName);
         for (int i = 0; i < employeeList.size(); i++) {
             if (employeeList.get(i).equals(emp)) {
-                System.out.println("Сотрудник с именем " + firstName + " и фамилией " + lastName + " найден: " + employeeList.get(i));
-                return true;
+                employeeList.remove(employeeList.get(i));
+                return;
             }
-            //System.out.println(emp.getFirstName() + " " + emp.getLastName() + " не найден!");
 
             throw new EmployeeNotFoundException();
 
         }
+    }
 
-        return false;
+    public Employee find(String firstName, String lastName) {
+        Employee emp = new Employee(firstName, lastName);
+        for (int i = 0; i < employeeList.size(); i++) {
+            if (employeeList.get(i).equals(emp)) {
+
+                return emp;
+            }
+        }
+
+        throw new EmployeeNotFoundException();
+
+    }
+
+    public Collection getAll(){
+        return employeeList;
     }
 }
